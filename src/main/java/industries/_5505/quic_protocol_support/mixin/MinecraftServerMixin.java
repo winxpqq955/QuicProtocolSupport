70ded3701aedc3daedb7d5195823ae2b3fdfd9b3
tesinormed <tesinormed@5505.industries>
2025-06-11 22:44:33 -0700

    version 0.4.4

---
+++ src/main/java/industries/_5505/quic_protocol_support/mixin/MinecraftServerMixin.java
@@ -0,0 +1,22 @@
+package industries._5505.quic_protocol_support.mixin;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.PlayerManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftServer.class)
+public class MinecraftServerMixin {
+	@Shadow
+	private PlayerManager playerManager;
+
+	@Inject(method = "shutdown", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/ServerNetworkIo;stop()V", shift = At.Shift.BEFORE))
+	private void shutdown(CallbackInfo callbackInfo) {
+		if (this.playerManager != null) {
+			this.playerManager.disconnectAllPlayers();
+		}
+	}
+}
